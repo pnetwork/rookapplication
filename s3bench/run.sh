9814aa706007ff3350e55077e81a12881059c0fc
jonah <jonah@jonahde-MacBook-Pro.local>
2019-02-21 13:08:23 +0800

    modify run.sh with secrets instead of secret

--- s3bench/run.sh
+++ s3bench/run.sh
@@ -3,9 +3,9 @@ threads=10
 loops=10
 bucket="s3-benchmark"
 access_key="CXPS27F2AWRVHJGDVGXA"
-secret_key="FCBqA35AGMsG5bPWdD1mA6Nfl8yV82iSeA6K4Ca7"
+secrets_key="FCBqA35AGMsG5bPWdD1mA6Nfl8yV82iSeA6K4Ca7"
 target="127.0.0.1:80"
 objkey="objkey"
 filesize=20000000
-./s3-bench -y $filesize -k $objkey -t $threads -l $loops  -a $access_key -b $bucket -s $secret_key -u $target 
+./s3-bench -y $filesize -k $objkey -t $threads -l $loops  -a $access_key -b $bucket -s $secrets_key -u $target 
 
